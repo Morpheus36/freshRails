d4a8a69d8e9ff5a82d36cf2bc0f93604a1a2ba05
Morpheus36 <shakedownzz201@gmail.com>
2019-04-21 17:48:44 +0100

    created seeds file for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+	Blog.create!(
+			title:"My Blog #{blog}",
+			body: "Some great text typed out by an average computer guy"
+		)
+
+end
+
+puts '10 blog posts created'
+
+5.times do |skill|
+	Skill.create!(
+		title:"Rails #{skill}",
+		percent_utilized: 15
+		)
+
+	end
+
+	puts '5 skills created'
+
+9.times do |portfolio_item|
+	Portfolio.create!(
+		title:"Porfolio TItle #{portfolio_item}",
+		subtitle:"my great service",
+		body:'Some more great text typed by ana average computer student',
+		main_image:"https://place-hold.it/300",
+		thum_image:"https://place-hold.it/200",
+		)
+
+end
+
+puts '9 portfolio_items created'
